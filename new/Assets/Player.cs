7821f9ddd32e37c3992da2e6358e9ae51a7edebb
Claudio Lopes <claudiolopesbore@hotmail.com>
2022-10-29 14:25:52 -0300

    commit 1

---
+++ new/Assets/Player.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    [SerializeField]
+    private Rigidbody2D m_rigidbody;
+
+    [SerializeField]
+    private float m_speed, m_jumpForce;
+
+    [SerializeField]
+    private Animator m_anim;
+
+    private bool isJumping;
+
+
+
+    private void FixedUpdate()
+    {
+        if (!isJumping)
+        {
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.localScale = Vector3.one;
+                //m_rigidbody.MovePosition(transform.position + m_speed * Time.deltaTime * Vector3.right);
+                m_anim.SetFloat("Speed", 1);
+                m_rigidbody.velocity = Vector2.right * m_speed;
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                transform.localScale = new Vector3(-1, 1, 1);
+                m_anim.SetFloat("Speed", 1);
+                m_rigidbody.velocity = Vector2.left * m_speed;
+                //m_rigidbody.MovePosition(transform.position + m_speed * Time.deltaTime * Vector3.left);
+            }
+            else
+            {
+                m_rigidbody.velocity = Vector2.zero;
+                m_anim.SetFloat("Speed", 0);
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
+        {
+            isJumping = true;
+            m_rigidbody.AddForce(Vector2.up * m_jumpForce);
+            m_anim.SetBool("IsJumping", true);
+            StartCoroutine(CheckGround());
+        }
+
+    }
+
+    IEnumerator CheckGround()
+    {
+        yield return new WaitForSeconds(.1f);
+        while (isJumping)
+        {
+            if (m_rigidbody.IsTouchingLayers(LayerMask.GetMask("Ground")))
+            {
+                m_anim.SetBool("IsJumping", false);
+                isJumping = false;
+            }
+            yield return null;
+        }
+    }
+
+
+}
---
+++ new/Assets/personagem.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class personagem : MonoBehaviour
+{
+    [SerializeField]
+    private Animator anime, animeDorsal;
+    private Rigidbody2D body;
+    public float jumpForce;
+    public float speed;
+	public int score;
+	public Text points;
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+		score = 0;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        if (Input.GetAxis("Horizontal") != 0.0f)
+            {
+                transform.localScale = new Vector3(Input.GetAxis("Horizontal")>0?1:-1, 1f, 1f);
+                body.velocity = new Vector2 (Input.GetAxis("Horizontal")*speed*Time.fixedDeltaTime, 0);
+                
+                //transform.Translate(speed*Input.GetAxis("Horizontal"), 0.0f, 0.0f);
+            }
+    }
+
+    void Update()
+    {
+        //Se estiver vivo
+        if (anime.GetInteger("Life") > 0)
+        {
+            //Idle -> Walk -> Run
+            
+            //Jump
+            if (Input.GetKeyDown("space") && anime.GetBool("Grounded"))
+            {
+                body.AddForce(new Vector2(0.0f, jumpForce));
+                anime.SetBool("Grounded", false);
+                anime.SetBool("Jump", true);
+            }
+            //Dead
+            if (Input.GetKey("z"))
+            {
+                anime.SetInteger("Life", 0);
+            }
+        }
+        //Se estiver morto
+        else anime.SetInteger("Life", -1);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            anime.SetBool("Grounded", true);
+            anime.SetBool("Jump", false);
+        }
+		/*if(collision.gameObject.CompareTag("Coin")){
+			Destroy(collision.gameObject);
+			score++;
+			points.text = "pontos:" + score;
+			print (score);
+		}*/
+	}
+
+}
